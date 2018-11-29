7586fa36972856fdcb5c1b586d86bbdf2d454bac
johan <johan.vorster91@gmail.com>
2018-11-29 18:15:11 +0200

    adding framework

--- MallAndCityApp/MasterViewController.m
+++ MallAndCityApp/MasterViewController.m
@@ -8,38 +8,36 @@
 
 #import "MasterViewController.h"
 #import "DetailViewController.h"
-
+#import <MallCitySDK/MallAndCityHttp.h>
+#import <MallCitySDK/MallAndCityUtil.h>
 @interface MasterViewController ()
 
 @property NSMutableArray *objects;
 @end
 
 @implementation MasterViewController
-
+MallAndCityUtil* util;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
-    self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    
+    MallAndCityHttp* httpRequest = [[MallAndCityHttp alloc] init];
+    util = [[MallAndCityUtil alloc] init];
+    [util setData:[httpRequest request:@"http://www.mocky.io/v2/5b7e8bc03000005c0084c210"]];
+    
+    if (!self.objects) {
+        self.objects = [[NSMutableArray alloc] init];
+    }
+    int i=0;
+    for (NSMutableArray *city in [util getCities:nil]) {
+        [self.objects insertObject:city atIndex:i];
+        i++;
+    }
 }
 
 
 - (void)viewWillAppear:(BOOL)animated {
     self.clearsSelectionOnViewWillAppear = self.splitViewController.isCollapsed;
     [super viewWillAppear:animated];
-}
-
-
-- (void)insertNewObject:(id)sender {
-    if (!self.objects) {
-        self.objects = [[NSMutableArray alloc] init];
-    }
-    [self.objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 
